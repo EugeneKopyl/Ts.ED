c6a59b412bbf8f8cf7474b1453982c72c1911e41
eugene.kopyl <eugene.kopyl@vrpconsulting.com>
2022-07-27 13:13:57 +0300

    Added http methods

--- src/controllers/offers/offers.controller.ts
+++ src/controllers/offers/offers.controller.ts
@@ -1,8 +1,9 @@
 import { Controller } from "@tsed/di";
-import { PathParams } from "@tsed/platform-params";
-import { Get } from "@tsed/schema";
+import { BodyParams, PathParams } from "@tsed/platform-params";
+import { Delete, Get, Post, Put } from "@tsed/schema";
 import { Offers } from "../../entities/Offers";
 import { OffersService } from "../../services/offers.service";
+import bodyParser from "body-parser";
 
 @Controller("/offers")
 export class OffersCtrl {
@@ -16,5 +17,23 @@ export class OffersCtrl {
   @Get("/:id")
   getById(@PathParams("id") id: string): Promise<Offers | null> {
     return this.offersService.findById(id);
+  }
+
+  @Put("/:id")
+  updateById(
+    @PathParams("id") id: string,
+    @BodyParams() offer: Offers
+  ): Promise<Offers> {
+    return this.offersService.updateById(id, offer);
+  }
+
+  @Post("/")
+  create(@BodyParams() offer: Offers): Promise<Offers> {
+    return this.offersService.create(offer);
+  }
+
+  @Delete("/:id")
+  deleteById(@PathParams("id") id: string): Promise<string> {
+    return this.offersService.deleteById(id);
   }
 }
--- src/services/offers.service.ts
+++ src/services/offers.service.ts
@@ -1,4 +1,6 @@
 import { OfferRepository } from "../repositories/OfferRepository";
+import { Offers } from "../entities/Offers";
+import { MssqlDataSource } from "../datasources/mssql.datasource";
 
 export class OffersService {
   async getAll() {
@@ -10,6 +12,32 @@ export class OffersService {
     const offer = OfferRepository.createQueryBuilder("offer")
       .where("offer.id = :id", { id: id })
       .getOne();
+    return offer;
+  }
+
+  async updateById(id: string, offer: Offers) {
+    await MssqlDataSource.createQueryBuilder()
+      .update(Offers)
+      .set(offer)
+      .where("id = :id", { id: id })
+      .execute();
+    return offer;
+  }
+
+  async deleteById(id: string) {
+    await MssqlDataSource.createQueryBuilder()
+      .delete()
+      .from(Offers)
+      .where("id = :id", { id: id })
+      .execute();
+    return "deleted";
+  }
+  async create(offer: Offers) {
+    await MssqlDataSource.createQueryBuilder()
+      .insert()
+      .into(Offers)
+      .values(offer)
+      .execute();
     return offer;
   }
 }
